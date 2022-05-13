b82c533ca408f4bb705c54ef1e70e2833afe5376
Oswaldo Paulo <oswaldo.paulo@gmail.com>
2022-05-13 12:32:40 -0300

    13-05-2022

---
+++ src/FacaEnquanto.java
@@ -0,0 +1,9 @@
+public class FacaEnquanto {
+    public static void main(String[] args){
+        int i = 0;
+        while(i != 10){
+            i++;
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/FacaEnquantoBreak.java
@@ -0,0 +1,12 @@
+public class FacaEnquantoBreak {
+    public static void main(String[] args){
+        int i = 0;
+         do{
+            i++;
+            System.out.println(i);
+            if(i > 10) {
+                break;
+            }
+        }while(true);
+    }
+}
